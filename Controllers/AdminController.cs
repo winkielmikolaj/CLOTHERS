a803dc001370620b1dc40f12c30c2c899497fd05
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-01-15 22:40:26 +0100

    Added admin panel with admin role and visability authorization

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Clothers.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clothers.Controllers
+{
+    [Authorize(Roles = Roles.Admin)]
+    public class AdminController : Controller
+    {
+        public IActionResult AdminPanel()
+        {
+            return View();
+        }
+    }
+}
