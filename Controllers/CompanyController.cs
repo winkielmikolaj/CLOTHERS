ab998e84a0bd276441eeab56aadcd2666474df46
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-01-16 10:22:11 +0100

    MyOffers view only showing a now logged in company offers

--- Controllers/CompanyController.cs
+++ Controllers/CompanyController.cs
@@ -1,16 +1,36 @@
 ﻿using Clothers.Constants;
+using Clothers.Data;
+using Clothers.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Clothers.Controllers
 {
+    [Authorize(Roles = Roles.Company)]
     public class CompanyController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        [Authorize(Roles = Roles.Company)]
-        public IActionResult CompanyPanel()
+        public CompanyController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
-            return View();
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> CompanyPanel()
+        {
+            var userId = _userManager.GetUserId(User);
+
+            var userOffers = await _context.Products
+                .Where(p => p.UserId == userId)
+                .ToListAsync();
+
+            return View(userOffers);
         }
     }
 }
