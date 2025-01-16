cb7e5f695eb452ad3f9e6a49a588eee7de728271
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-01-16 10:07:29 +0100

    Added company panel with comapny role and visability authorization

---
+++ Controllers/CompanyController.cs
@@ -0,0 +1,16 @@
+﻿using Clothers.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clothers.Controllers
+{
+    public class CompanyController : Controller
+    {
+
+        [Authorize(Roles = Roles.Company)]
+        public IActionResult CompanyPanel()
+        {
+            return View();
+        }
+    }
+}
